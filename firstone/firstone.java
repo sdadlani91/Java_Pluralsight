46f782341f947103f4d88040bf2016acfdd763fa
Shaan Dadlani <sdadlani@redventures.com>
2019-04-06 10:40:47 -0400

    Adding folder and package

---
+++ firstone/firstone.java
@@ -0,0 +1,28 @@
+
+
+package sdadlani.pluralsight.java
+
+public class firstone
+{
+
+    public static void main(String[] args)
+
+        {
+
+            int a = 36, b=6, c=3;
+    
+            int result2 = (a-b)/c;
+            int result1 ;
+            for(c=3;c<10;++c)
+       
+                //System.out.println("Oh shittttt, this be a computer program"+((a-b)/c)+" some other number shit"+c);
+
+        }
+
+        System.out.print(a);
+
+
+
+
+
+}
